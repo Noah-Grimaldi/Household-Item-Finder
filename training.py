1be5c05ab6ca774e7f30312f3d02ad4d32fe07bf
Noah-Grimaldi <noahgrimaldi1@gmail.com>
2025-02-25 14:26:38 -0500

    setting up new project

--- training.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.yaml")  # path to your YAML config file
-results = model.train(data=r"path\to\your\dataset.yolov8\data.yaml", epochs=50)  # path to your dataset YAML file
